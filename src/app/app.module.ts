7937aa7edfa1ae57968f2bf0a9158e0794d52312
jesus diaz <jesus.diaz@hotmail.com>
2019-05-22 15:54:15 -0500

    se inplementan  validaciones   del login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { ErrorPageComponent } from './error-page/error-page.component';
 import { HomeComponent } from './home/home.component';
+import {  ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private formBuilder: FormBuilder) { }
+   validateForm:FormGroup;
+   Clicksubmitted=false;
+ 
   ngOnInit() {
-  }
+    this.validateForm = this.formBuilder.group({   
+      email: ['', [Validators.required,Validators.email]],
+      inputPassword: ['', [Validators.required, Validators.minLength(6)]]
+
+  });
+
+}
+
+get f() { return this.validateForm.controls; }
+
+onSubmit(){
+  this.Clicksubmitted=true;
+  
+        // stop here if form is invalid
+        if (this.validateForm.invalid) {
+              return;
+      }
+
+      alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.validateForm.value))
+
+
+}
+
+
 
 }
